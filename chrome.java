2f1fa1da1797b93cf12e697d0f0484edf2384455
cpremkumar4709 <cpremkumar4709@gmail.com>
2025-07-19 18:02:45 +0530

    Add files via upload
    Week1Day1 code Commit

---
+++ chrome.java
@@ -0,0 +1,25 @@
+
+public class chrome 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		float version = 91.0f;
+		System.out.println(version);
+		
+		String developer = "Google";
+		System.out.println(developer);
+		
+		boolean isBeta = false;
+		System.out.println(isBeta);
+		
+		int releaseyear = 2008;
+		System.out.println(releaseyear);
+		
+		char shortcutkey = 'C';
+		System.out.println(shortcutkey);
+
+	}
+
+}
