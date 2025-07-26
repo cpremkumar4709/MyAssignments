72929d27fea52f1a858e2170bcd573d3c4490b71
cpremkumar4709 <cpremkumar4709@gmail.com>
2025-07-26 09:00:11 +0530

    Add files via upload

---
+++ assignmentWeek1day2/CheckNumberIsPositive.java
@@ -0,0 +1,22 @@
+package assignmentWeek1day2;
+
+public class CheckNumberIsPositive 
+{
+
+	public static void main(String[] args) 
+	{
+		int number=10;
+		if(number>0)
+		{
+			System.out.println("Number is Positive");
+		}
+		else
+		{
+			{
+				System.out.println("Number is Positive");
+			}
+		}
+		
+	}
+
+}
